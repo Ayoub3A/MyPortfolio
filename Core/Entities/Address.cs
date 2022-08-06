2bf30f41f8844673cd6465d55ec04a6351d13be1
Ayoub AITALLA <aitallaayoub@gmail.com>
2022-08-06 16:05:25 +0200

    Ajoutez des fichiers projet.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    // to use tables let's consider that the owner have an address
+    public class Address : EntityBase
+    {
+        public string Street { get; set; }
+        public string city { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/Entities/Owner.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities
+{
+    public class Owner : EntityBase
+    {
+        public string FullName { get; set; }
+        public string Profil { get; set; }
+        // for the avatar we will save only the path of the picture and the picture will be saved on the server
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+}
